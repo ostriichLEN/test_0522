0f968a14976d12c240a555bfcfb12324e3e8305b
“ostriichLEN” <james205032889@gmail.com>
2026-05-22 11:36:58 +0800

    Update calculate.py

--- src/calculate.py
+++ src/calculate.py
@@ -1,3 +1,8 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
 def add_func(a, b):
     return a + b
 
@@ -5,3 +10,18 @@ def add_func(a, b):
 def sub_func(a, b):
     # temp = 123
     return a - b
+
+
+def mul_func(a: float, b: float) -> float:
+    return a * b
+
+
+@app.get("/")
+def home():
+    return {"status": "online", "message": "Welcome to the calculator API!"}
+
+
+@app.get("/add")
+def calculate_add(a: float, b: float):
+    result = add_func(a, b)
+    return {"operation": "addition", "a": a, "b": b, "result": result}
